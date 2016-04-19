be4fe076e1c0f4ceb305aaebf7e20545aaf66446
StephDungan <b00092222@student.itb.ie>
2016-04-19 16:47:12 +0100

    Added files via upload
    hard coded login & navbar file

---
+++ templates/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stephanie Dungan
+ * Date: 18/03/2016
+ * Time: 11:41
+ */
+require_once __DIR__ . '/../templates/header1.inc.php';
+require_once __DIR__ . '/../templates/nav.inc.php';
+
+?>
+
+
+<?php
+require_once  __DIR__. '/../templates/footer.inc.php';
